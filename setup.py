ed7f9f6d510b09e35c312d697784ab08d7829683
Ramon Rodrigues <ramon.srodrigues01@gmail.com>
2020-04-09 23:05:54 -0300

    Update name package

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
 
 
 def get_version(*file_paths):
-    """Retrieves the version from django_charts/__init__.py"""
+    """Retrieves the version from dj_chartjs/__init__.py"""
     filename = os.path.join(os.path.dirname(__file__), *file_paths)
     version_file = open(filename).read()
     version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
@@ -21,7 +21,7 @@ def get_version(*file_paths):
     raise RuntimeError('Unable to find version string.')
 
 
-version = get_version("django_charts", "__init__.py")
+version = get_version("dj_chartjs", "__init__.py")
 
 
 if sys.argv[-1] == 'publish':
@@ -45,7 +45,7 @@ readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
-    name='django-charts',
+    name='dj-chartjs',
     version=version,
     description="""Library to render chart using chartjs""",
     long_description=readme + '\n\n' + history,
@@ -53,13 +53,13 @@ setup(
     author_email='ramon.srodrigues01@gmail.com',
     url='https://github.com/ramon5/django-charts',
     packages=[
-        'django_charts',
+        'dj_chartjs',
     ],
     include_package_data=True,
     install_requires=[],
     license="MIT",
     zip_safe=False,
-    keywords='django-charts',
+    keywords='dj-chartjs',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Framework :: Django :: 1.11',
