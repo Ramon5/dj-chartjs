a8913cde976c962cff9114c8d223dd6074cbd2e1
Ramon Rodrigues <ramon.srodrigues01@gmail.com>
2020-04-12 22:35:09 -0300

    Fixed color variable in views.py

--- dj_chartjs/views.py
+++ dj_chartjs/views.py
@@ -39,7 +39,7 @@ class BaseChartView(ABC):
         )
 
     def _get_rgba_from_hex(self, color_hex):
-        color = color.lstrip("#")
+        color = color_hex.lstrip("#")
         rgb = [int(color[i : i + 2], 16) for i in [0, 2, 4]]
 
         return "rgba({},{},{}, 0.4)".format(*map(lambda x: rgb))
